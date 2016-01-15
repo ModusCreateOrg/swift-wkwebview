4d8ec397347833348b7d6e7c2c28810b28748f9d
Michael Schwartz <mykesx@gmail.com>
2016-01-15 10:28:03 -0800

    step 1 complete

--- swift-wkwebview/ViewController.swift
+++ swift-wkwebview/ViewController.swift
@@ -10,23 +10,21 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // create a WKWebView full size of the screen, minus 20 pixels for the top status bar
         let screenSize: CGRect = UIScreen.mainScreen().bounds
         let frameRect: CGRect = CGRect(x: 0, y: 20, width: screenSize.width, height: screenSize.height)
-
         self.webView = WKWebView(frame: frameRect)
 
-        // tell webview that we are the UIDelegate to handle alert(), confirm(), etc.
-        self.webView.UIDelegate = self
-
-        self.webView.configuration.preferences.setValue(true, forKey: "developerExtrasEnabled")
+        // add WKWebView to the display
         self.view.addSubview(self.webView)
 
+        // load dist/index.html into the WKWebView
         let url: NSURL! = NSBundle.mainBundle().URLForResource("dist/index", withExtension: "html")
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
@@ -39,15 +37,5 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    func webView(webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: (() -> Void)) {
-        print("webView:\(webView) runJavaScriptAlertPanelWithMessage:\(message) initiatedByFrame:\(frame) completionHandler:\(completionHandler)")
-
-        let alertController = UIAlertController(title: frame.request.URL?.host, message: message, preferredStyle: .Alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: { action in
-            completionHandler()
-        }))
-        self.presentViewController(alertController, animated: true, completion: nil)
     }
 }
